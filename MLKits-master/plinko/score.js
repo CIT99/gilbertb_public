8a7aa2a2f9dcdb5752b3dd86e2e30f7f53cbe53f
Gilbert Barr <gbarr6622@gmail.com>
2019-10-28 00:38:27 -0700

    Score.js updated to video lecture 21

--- MLKits-master/plinko/score.js
+++ MLKits-master/plinko/score.js
@@ -1,13 +1,62 @@
 
 const outputs = [];
+//const predictionPoint = 300;
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-  console.log(outputs);
+
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
+
+  let testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs,testSetSize);
+  
+  /*let numCorrect = 0;
+  for(let i=0; i < testSet.length; i++){
+    const bucket = knn(trainingSet,testSet[i][0]);
+    //console.log(bucket,testSet[i][3]);
+    if(bucket === testSet[i][3]){
+      numCorrect++;
+    }
+  }
+  console.log('Accuracy: ', numCorrect/testSetSize);
+  */
+
+  const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet,testPoint[0]) === testPoint[3])
+    .size()
+    .divide(testSetSize)
+    .value()
+
+  console.log('Accuracy is', accuracy);
+
 }
 
+function knn (data, testPoint){
+  return _.chain(data)
+    .map(row => [distance(row[0],testPoint), row[3]])
+    .slice(0,k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+}
+
+function distance(piont,testPoint){
+  return Math.abs(piont - testPoint)
+}
+
+function splitDataset(data, testCount){
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const  trainingSet = _.slice(shuffled,testCount);
+
+  return [testSet, trainingSet];
+}
